56d53194e169f7d4cb70f7dbc2dfccbcd0e1977e
Bolanle Adisa <bolanleadisa6718@gmail.com>
2024-01-22 11:18:14 -0600

    Initial Commit

---
+++ clearoutt/clearouttApp.swift
@@ -0,0 +1,17 @@
+//
+//  clearouttApp.swift
+//  clearoutt
+//
+//  Created by Bolanle Adisa on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct clearouttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
